164f0c928a487595553282920c4326c95412d894
Adam Hukalowicz <adam.hukalowicz@turbinekreuzberg.com>
2022-07-05 17:04:50 +0200

    wip

--- pkg/cluster.go
+++ pkg/cluster.go
@@ -2,10 +2,16 @@ package k8status
 
 import (
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	"path/filepath"
 )
+
+type KubeClient struct {
+	clientset  *kubernetes.Clientset
+	restconfig *rest.Config
+}
 
 func GetClientSet() (*kubernetes.Clientset, error) {
 	home, err := os.UserHomeDir()
@@ -13,7 +19,7 @@ func GetClientSet() (*kubernetes.Clientset, error) {
 		return nil, err
 	}
 
-	kubeconfigFile := filepath.Join(home, ".kube", "nautilus")
+	kubeconfigFile := filepath.Join(home, ".kube", "config")
 
 	kubeconfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigFile)
 	if err != nil {
@@ -26,4 +32,30 @@ func GetClientSet() (*kubernetes.Clientset, error) {
 	}
 
 	return clientset, nil
+}
+
+func GetKubeClient() (*KubeClient, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	kubeconfigFile := filepath.Join(home, ".kube", "config")
+
+	kubeconfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigFile)
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &KubeClient{
+		clientset:  clientset,
+		restconfig: kubeconfig,
+	}
+
+	return client, nil
 }
