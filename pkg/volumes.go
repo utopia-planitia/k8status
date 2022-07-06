825cab1ab1f11e6d057402f707a461f84637e24c
Adam Hukalowicz <adam.hukalowicz@turbinekreuzberg.com>
2022-07-06 16:42:10 +0200

    volumes added

---
+++ pkg/volumes.go
@@ -0,0 +1,26 @@
+package k8status
+
+import (
+	"context"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func PrintVolumeStatus(ctx context.Context, clientset *kubernetes.Clientset, verbose bool) error {
+	pv, err := clientset.CoreV1().PersistentVolumes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	healthy := 0
+	for _, item := range pv.Items {
+		if item.Status.Phase == v1.VolumeBound || item.Status.Phase == v1.VolumeAvailable {
+			healthy++
+		}
+	}
+	fmt.Printf("%d of %d volumes are bound or available", healthy, len(pv.Items))
+
+	return nil
+}
