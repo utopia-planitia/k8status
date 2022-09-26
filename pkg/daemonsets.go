e46d1e5cc3a90bc2e2210abbacd1705f53a4f31a
Mário Saleiro <mario.saleiro@turbinekreuzberg.com>
2022-09-26 17:06:07 +0100

    reworked printdaemonsetstatus to use tableable

--- pkg/daemonsets.go
+++ pkg/daemonsets.go
@@ -10,6 +10,24 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type daemonsetTableView struct {
+	name      string
+	namespace string
+	scheduled string
+	current   string
+	ready     string
+	updated   string
+	available string
+}
+
+func (c daemonsetTableView) header() []string {
+	return []string{"Daemonset", "Namespace", "Scheduled", "Current", "Ready", "Up-to-date", "Available"}
+}
+
+func (c daemonsetTableView) row() []string {
+	return []string{c.name, c.namespace, c.scheduled, c.current, c.ready, c.updated, c.available}
+}
+
 func PrintDaemonsetStatus(ctx context.Context, header io.Writer, details colorWriter, client *KubernetesClient, verbose bool) (int, error) {
 	daemonsets, err := client.clientset.AppsV1().DaemonSets("").List(ctx, metav1.ListOptions{})
 	_ = daemonsets
@@ -19,7 +37,8 @@ func PrintDaemonsetStatus(ctx context.Context, header io.Writer, details colorWr
 
 	healthy := 0
 	total := 0
-	table, err := CreateTable(details, []string{"Daemonset", "Namespace", "Scheduled", "Current", "Ready", "Up-to-date", "Available"}, tablewriter.FgYellowColor)
+
+	table, err := CreateTable(details, tableHeader(daemonsetTableView{}), tablewriter.FgYellowColor)
 	if err != nil {
 		return 0, err
 	}
@@ -34,9 +53,10 @@ func PrintDaemonsetStatus(ctx context.Context, header io.Writer, details colorWr
 			item.Status.DesiredNumberScheduled == item.Status.NumberAvailable {
 			healthy++
 		} else {
-			tableData = append(tableData, []string{item.Name, item.Namespace, fmt.Sprintf("%d", item.Status.DesiredNumberScheduled),
+			tableData = append(tableData, tableRow(daemonsetTableView{item.Name, item.Namespace, fmt.Sprintf("%d", item.Status.DesiredNumberScheduled),
 				fmt.Sprintf("%d", item.Status.CurrentNumberScheduled), fmt.Sprintf("%d", item.Status.NumberReady),
-				fmt.Sprintf("%d", item.Status.UpdatedNumberScheduled), fmt.Sprintf("%d", item.Status.NumberAvailable)})
+				fmt.Sprintf("%d", item.Status.UpdatedNumberScheduled), fmt.Sprintf("%d", item.Status.NumberAvailable)}))
+
 		}
 
 	}
